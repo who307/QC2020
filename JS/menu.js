98461fd6c92badb66472a75ba89205f391179498
moon gun <who307@naver.com>
2020-09-19 15:59:22 +0900

    9월 19일 작업

--- JS/menu.js
+++ JS/menu.js
@@ -10,3 +10,34 @@ function toggleMenu(){
 
 }
 toggleBtn.addEventListener("click", toggleMenu);
+
+// 스크롤
+$(function(){
+  var header = $('header');
+  var page = $('.article2');
+  var menu =$('.menu');
+    
+  var pageOffsetTop = page.offset().top;
+  $(window).resize(function(){ 
+    pageOffsetTop = page.offset().top;
+  });
+    
+  $(window).on('scroll', function(){
+    if($(window).scrollTop() >= pageOffsetTop) {
+      header.addClass('down');
+      menu.addClass('black');
+
+    } else { 
+      header.removeClass('down');
+      menu.removeClass('black');
+    }
+  });
+});
+
+jQuery(document).ready(function ($) {
+
+  $(".scroll").click(function (event) {
+    event.preventDefault();
+    $('html,body').animate({ scrollTop: $(this.hash).offset().top }, 800);
+  });
+});
